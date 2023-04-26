dab425c95ef196122463abecacd287e1e2db9893
김진호 <wlsghsla94@naver.com>
2023-04-26 23:36:18 +0900

    feat: SwaggerConfig 작성

--- backend/spring/src/main/java/com/ssafy/config/SwaggerConfig.java
+++ backend/spring/src/main/java/com/ssafy/config/SwaggerConfig.java
@@ -1,14 +1,7 @@
 package com.ssafy.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,56 +10,30 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+// config/SwaggerConfig.java
 @Configuration
-@EnableAsync
-@EnableWebMvc
 @EnableSwagger2
-public class WebMvcConfiguration implements WebMvcConfigurer {
+public class SwaggerConfig {  // Swagger
 
-    // ... 중략 ...
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
+    private static final String API_NAME = "66days API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "66days API 명세서";
 
     @Bean
-    public Docket swagger() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.66days.book.springboot"))  // Swagger를 적용할 클래스의 package명
+                .paths(PathSelectors.any())  // 해당 package 하위에 있는 모든 url에 적용
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {  // API의 이름, 현재 버전, API에 대한 정보
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
                 .build();
     }
 }
-//@OpenAPIDefinition(
-//        info = @Info(title = "채팅서비스 API 명세서",
-//                description = "헥사고날 아키텍처 기반 채팅 서비스 API 명세서",
-//                version = "v1"))
-//@Configuration
-//@RequiredArgsConstructor
-//@EnableSwagger2
-//public class SwaggerConfig {
-//    private static final String API_NAME = "66days API";
-//    private static final String API_VERSION = "0.0.1";
-//    private static final String API_DESCRIPTION = "66days API 명세서";
-//
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.66days.book.springboot"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
-//    public ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title(API_NAME)
-//                .version(API_VERSION)
-//                .description(API_DESCRIPTION)
-//                .build();
-//    }
-//}
