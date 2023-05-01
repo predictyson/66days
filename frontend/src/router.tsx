1065859738589ed03dd395d07ee5b0bd7e14dcf7
손예지 <yejison98@gmail.com>
2023-05-01 12:49:06 +0900

    feat: landing #1 content

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -6,12 +6,14 @@ import {
 import ErrorPage from "./error-page";
 import Root from "./routes/Root";
 import Home from "./routes/Home";
+import LandingPage from "./routes/LandingPage";
 
 export default createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
       <Route errorElement={<ErrorPage />}>
         <Route index element={<Home />} />
+        <Route path="/landing" element={<LandingPage />} />
       </Route>
     </Route>
   )
---
+++ frontend/src/routes/LandingPage.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../styles/theme";
+export default function LandingPage() {
+  return (
+    <>
+      <Container>
+        <LeftWrapper>
+          <Title>
+            성장하는 개발자가 되기 위한 <br /> <a>66일 </a>간의 여정
+          </Title>
+
+          <div className="subcontent" style={{ marginTop: "4.4rem" }}>
+            알고리즘 풀이, 기술 블로그 포스팅 ...
+            <br />
+            작심 삼일의 계획에 그치진 않았나요?
+          </div>
+          <div className="subcontent">
+            이제 <a>66days</a>가 도와줄게요 !
+          </div>
+          <JoinButton>66days 시작하기</JoinButton>
+        </LeftWrapper>
+        <GifWrapper></GifWrapper>
+      </Container>
+      <Container></Container>
+    </>
+  );
+}
+const Container = styled.div`
+  width: 100%;
+  height: 68.8rem;
+  display: flex;
+  padding: 0 7.5rem;
+  font-family: Pretendard;
+`;
+
+const Title = styled.div`
+  margin-top: 17rem;
+  font-size: 4.8rem;
+  font-family: Pretendard;
+  font-weight: ${theme.fontWeight.semibold};
+`;
+const LeftWrapper = styled.div`
+  width: 60%;
+  flex-direction: column;
+  .subcontent {
+    font-size: 2.4rem;
+    color: ${theme.colors.gray500};
+    margin-top: 2.2rem;
+    font-weight: ${theme.fontWeight.medium};
+  }
+  a {
+    color: ${theme.colors.purple};
+    font-weight: bold;
+  }
+`;
+
+const JoinButton = styled.div`
+  margin-top: 4.4rem;
+  padding: 0.7rem 1rem;
+  width: 31rem;
+  height: 6rem;
+  font-size: 2.8rem;
+  border-radius: 20px;
+  text-align: center;
+  background-color: ${theme.colors.purple};
+  color: white;
+  font-weight: ${theme.fontWeight.semibold};
+  transition: transform 0.3s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+    background-color: ${theme.colors.gray100};
+    color: ${theme.colors.purple};
+    border: solid 1px ${theme.colors.purple};
+  }
+`;
+const GifWrapper = styled.div`
+  width: 40%;
+  border: solid 1px red;
+`;
