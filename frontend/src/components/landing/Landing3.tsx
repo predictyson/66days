7af3615c72098d72ef1a3186970a12f783bd0f3a
손예지 <yejison98@gmail.com>
2023-05-01 15:01:39 +0900

    feat: landing page view

---
+++ frontend/src/components/landing/Landing3.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../styles/theme";
+import Vector from "../../assets/landing/landing3_vector.png";
+import View from "../../assets/landing/landing3_view.png";
+import LandingGif3 from "../../assets/landing/landing 2.gif";
+export default function Landing1() {
+  return (
+    <>
+      <Container>
+        <LeftWrapper>
+          <Title>그룹 형성과 챌린지 생성</Title>
+          <img
+            src={View}
+            alt="view"
+            style={{ width: "41.3rem", margin: "2rem auto", display: "flex" }}
+          />
+        </LeftWrapper>
+        <GifWrapper>
+          <img src={LandingGif3} alt="landing1" />
+        </GifWrapper>
+      </Container>
+    </>
+  );
+}
+const Container = styled.div`
+  width: 100%;
+  height: 68.8rem;
+  display: flex;
+  padding: 0 7.5rem;
+  font-family: Pretendard;
+`;
+
+const Title = styled.div`
+  margin-top: 15rem;
+  font-size: 4rem;
+  font-weight: ${theme.fontWeight.bold};
+`;
+const LeftWrapper = styled.div`
+  width: 50%;
+  padding: 0 10rem;
+  img {
+    transition: transform 0.3s ease-in-out;
+    cursor: pointer;
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`;
+
+const GifWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${Vector});
+  background-position: center;
+  background-repeat: no-repeat;
+`;
--- frontend/src/routes/LandingPage.tsx
+++ frontend/src/routes/LandingPage.tsx
@@ -1,87 +1,15 @@
 import React from "react";
-import styled from "styled-components";
-import { theme } from "../styles/theme";
-import LandingGif1 from "../assets/landing/landing1.gif";
+import Landing1 from "../components/landing/Landing1";
+import Landing2 from "../components/landing/Landing2";
+import Landing3 from "../components/landing/Landing3";
+import Landing4 from "../components/landing/Landing4";
 export default function LandingPage() {
   return (
     <>
-      <Container>
-        <LeftWrapper>
-          <Title>
-            성장하는 개발자가 되기 위한 <br /> <a>66일 </a>간의 여정
-          </Title>
-
-          <div className="subcontent" style={{ marginTop: "4.4rem" }}>
-            알고리즘 풀이, 기술 블로그 포스팅 ...
-            <br />
-            작심 삼일의 계획에 그치진 않았나요?
-          </div>
-          <div className="subcontent">
-            이제 <a>66days</a>가 도와줄게요 !
-          </div>
-          <JoinButton>66days 시작하기</JoinButton>
-        </LeftWrapper>
-        <GifWrapper>
-          <img src={LandingGif1} alt="landing1" />
-        </GifWrapper>
-      </Container>
-      <Container></Container>
+      <Landing1 />
+      <Landing2 />
+      <Landing3 />
+      <Landing4 />
     </>
   );
 }
-const Container = styled.div`
-  width: 100%;
-  height: 68.8rem;
-  border: solid 1px black;
-  display: flex;
-  padding: 0 7.5rem;
-  font-family: Pretendard;
-`;
-
-const Title = styled.div`
-  margin-top: 15rem;
-  font-size: 4.8rem;
-  font-family: Pretendard;
-  font-weight: ${theme.fontWeight.semibold};
-`;
-const LeftWrapper = styled.div`
-  width: 60%;
-  flex-direction: column;
-  .subcontent {
-    font-size: 2.4rem;
-    color: ${theme.colors.gray500};
-    margin-top: 2.2rem;
-    font-weight: ${theme.fontWeight.medium};
-  }
-  a {
-    color: ${theme.colors.purple};
-    font-weight: bold;
-  }
-`;
-
-const JoinButton = styled.div`
-  margin-top: 4.4rem;
-  padding: 0.7rem 1rem;
-  width: 31rem;
-  height: 6rem;
-  font-size: 2.8rem;
-  border-radius: 20px;
-  text-align: center;
-  background-color: ${theme.colors.purple};
-  color: white;
-  font-weight: ${theme.fontWeight.semibold};
-  transition: transform 0.3s ease-in-out;
-  cursor: pointer;
-  &:hover {
-    transform: scale(1.1);
-    background-color: ${theme.colors.gray100};
-    color: ${theme.colors.purple};
-    border: solid 1px ${theme.colors.purple};
-  }
-`;
-const GifWrapper = styled.div`
-  width: 40%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
