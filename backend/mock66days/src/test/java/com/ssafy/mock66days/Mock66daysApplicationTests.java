8eff48b38fd9eeda59d4b5b144ef1739ef19e79d
권성은 <isungun@naver.com>
2023-05-04 12:57:11 +0900

    be/feat: spring boot test annotaion exclude

--- backend/mock66days/src/test/java/com/ssafy/mock66days/Mock66daysApplicationTests.java
+++ backend/mock66days/src/test/java/com/ssafy/mock66days/Mock66daysApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.mock66days;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Mock66daysApplicationTests {
 
 	@Test
