eefa07c2a234da39b62e285b25bda2d47f4dde8c
dan <dankim0213@gmail.com>
2023-05-07 21:48:00 +0900

    fe/feat: add Group-intro page

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -7,6 +7,7 @@ import ErrorPage from "./error-page";
 import Root from "./routes/Root";
 import LandingPage from "./routes/LandingPage";
 import Group from "./routes/Group";
+import GroupIntro from "./routes/GroupIntro";
 import Challenge from "./routes/Challenge";
 import MainPage from "./routes/MainPage";
 import SearchPage from "./routes/SearchPage";
@@ -20,7 +21,7 @@ export default createBrowserRouter(
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/groups" element={<SearchPage />} />
         <Route path="/groups/me" index element={<Group />} />
-        <Route path="/groups/:groupId" index element={<Group />} />
+        <Route path="/groups/:groupId" index element={<GroupIntro />} />
         <Route path="/groups/:groupId/c/:challengeId" element={<Challenge />} />
       </Route>
     </Route>
