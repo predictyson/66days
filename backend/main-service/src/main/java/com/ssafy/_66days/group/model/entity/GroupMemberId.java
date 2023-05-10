f9328bdec0c06355a59a23e63a7fb74240117249
김진호 <wlsghsla94@naver.com>
2023-05-10 13:43:27 +0900

    be/refactor: article, comment refactor 완료

--- backend/main-service/src/main/java/com/ssafy/_66days/group/model/entity/GroupMemberId.java
+++ backend/main-service/src/main/java/com/ssafy/_66days/group/model/entity/GroupMemberId.java
@@ -3,9 +3,13 @@ package com.ssafy._66days.group.model.entity;
 import com.ssafy._66days.group.model.entity.Group;
 import com.ssafy._66days.user.model.entity.User;
 
+import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.UUID;
 
+@Embeddable
 public class GroupMemberId implements Serializable {
-    private User user;
-    private Group group;
+
+    private UUID user;
+    private Long group;
 }
