c335801aa0b384ec3b719e72b7f538ed814883e7
권성은 <isungun@naver.com>
2023-05-14 17:43:59 +0900

    be/feat:test annotation disabled

--- backend/apigateway-service/src/test/java/com/ssafy/apigatewayservice/ApigatewayServiceApplicationTests.java
+++ backend/apigateway-service/src/test/java/com/ssafy/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.apigatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ApigatewayServiceApplicationTests {
 
 	@Test
--- backend/config-service/src/test/java/com/ssafy/configservice/ConfigServiceApplicationTests.java
+++ backend/config-service/src/test/java/com/ssafy/configservice/ConfigServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.configservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ConfigServiceApplicationTests {
 
 	@Test
