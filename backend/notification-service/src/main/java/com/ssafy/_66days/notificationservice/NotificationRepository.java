348afaecbfd6a5b5bdfda079c94fee1b2570a52d
권성은 <isungun@naver.com>
2023-05-15 20:47:56 +0900

    Resolve "noti-back"

---
+++ backend/notification-service/src/main/java/com/ssafy/_66days/notificationservice/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.ssafy._66days.notificationservice;
+
+import org.springframework.data.domain.Pageable;
+import reactor.core.publisher.Flux;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+
+
+public interface NotificationRepository extends ReactiveMongoRepository<Notification,String> {
+    @Query("{ 'user_id' : ?0 }")
+    Flux<Notification> mFindByUserId(Integer userId, Pageable pageable);
+}
