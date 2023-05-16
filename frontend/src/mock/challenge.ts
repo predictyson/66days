efa7af61e5b3542d602d395afcd00921be539d58
김태원 <taw4654@gmail.com>
2023-05-16 13:32:53 +0900

    fe/design: 챌린지 디자인 수정

--- frontend/src/mock/challenge.ts
+++ frontend/src/mock/challenge.ts
@@ -1,22 +1,26 @@
 const mockUsers = [
   {
-    name: "dfl;kajsdflk;sjladkfjl;asdkfjl;a",
+    name: "성다여니니",
+    profile: "https://xsgames.co/randomusers/avatar.php?g=pixel&key=2",
+  },
+  {
+    name: "뭉치",
+    profile: "",
+  },
+  {
+    name: "Jim Carry",
     profile: "https://xsgames.co/randomusers/avatar.php?g=pixel&key=3",
   },
   {
-    name: "dfl;kajsdflk;sjladkfjl;asdkfjl;a",
-    profile: "https://xsgames.co/randomusers/avatar.php?g=pixel&key=3",
-  },
-  {
-    name: "dfl;kajsdflk;sjladkfjl;asdkfjl;a",
+    name: "그댈떠나가는 그순간도",
     profile: "https://xsgames.co/randomusers/avatar.php?g=pixel&key=3",
   },
 ];
 
 const mockMessages = [
   {
-    nickname: "뭉치",
-    profile: "",
+    nickname: "성다여니니",
+    profile: "https://xsgames.co/randomusers/avatar.php?g=pixel&key=2",
     value: "피그마는 어때요?",
     date: "오후 8:30",
     me: false,
@@ -24,42 +28,28 @@ const mockMessages = [
   {
     nickname: "뭉치",
     profile: "",
-    value: ";lkasjdflk;jasdlkf;jasdklfj klas;djflkas",
+    value: "So far so good",
+    date: "오후 8:30",
+    me: true,
+  },
+  {
+    nickname: "성다여니니",
+    profile: "https://xsgames.co/randomusers/avatar.php?g=pixel&key=2",
+    value: "이번 챌린지는 너무 힘들어요 ㅜㅜ",
     date: "오후 8:30",
     me: false,
   },
   {
     nickname: "뭉치",
     profile: "",
-    value: ";lkasjdflk;jasdlkf;jasdklfj klas;djflkas",
+    value: "하지만 다연님과 함께라면 할 수 있어요!",
     date: "오후 8:30",
     me: true,
   },
   {
     nickname: "뭉치",
     profile: "",
-    value: ";lkasjdflk;jasdlkf;jasdklfj klas;djflkas",
-    date: "오후 8:30",
-    me: true,
-  },
-  {
-    nickname: "뭉치",
-    profile: "",
-    value: ";lkasjdflk;jasdlkf;jasdklfj klas;djflkas",
-    date: "오후 8:30",
-    me: true,
-  },
-  {
-    nickname: "뭉치",
-    profile: "",
-    value: ";lkasjdflk;jasdlkf;jasdklfj klas;djflkas",
-    date: "오후 8:30",
-    me: true,
-  },
-  {
-    nickname: "뭉치",
-    profile: "",
-    value: ";lkasjdflk;jasdlkf;jasdklfj klas;djflkas",
+    value: "나 권성은 할 수 있다!",
     date: "오후 8:30",
     me: true,
   },
@@ -121,126 +111,6 @@ const mockStreak = [
   {
     date: "2023-05-09",
     count: 1,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
-  },
-  {
-    date: "2023-05-09",
-    count: 3,
   },
   {
     date: "2023-05-09",
