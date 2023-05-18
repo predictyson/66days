ad1917e5d4e51e3a394625058cc556394f0e6b49
Da Yeon Sung <sdy32@naver.com>
2023-05-18 16:24:54 +0900

    fe/hotfix: api url 경로 변경

--- frontend/src/api/group.tsx
+++ frontend/src/api/group.tsx
@@ -7,7 +7,7 @@ interface Board {
 
 async function fetchGroupPageData(id = 1) {
   try {
-    const res = await api.get(`/api/v1/main-service/page/groups/${id}`);
+    const res = await api.get(`/api/v2/main-service/page/groups/${id}`);
     if (res.status === 200) {
       return res.data;
     }
@@ -23,7 +23,7 @@ async function fetchGroupPageData(id = 1) {
 async function fetchGroupMembers(id = 1) {
   try {
     const res = await api.get(
-      `/api/v1/main-service/group/${id}/manage/members`
+      `/api/v2/main-service/group/${id}/manage/members`
     );
     if (res.status === 200) {
       console.log(res.data);
@@ -40,7 +40,7 @@ async function fetchGroupMembers(id = 1) {
 // 그룹 가입 신청원 리스트 데이터 fetch
 async function fetchAppliedMembers(id = 1) {
   try {
-    const res = await api.get(`/api/v1/main-service/group/${id}/manage/apply`);
+    const res = await api.get(`/api/v2/main-service/group/${id}/manage/apply`);
     if (res.status === 200) {
       return res.data;
     }
@@ -54,7 +54,7 @@ async function fetchAppliedMembers(id = 1) {
 
 async function fetchGroupBadges(id = 1) {
   try {
-    const res = await api.get(`/api/v1/main-service/badge/list/${id}`);
+    const res = await api.get(`/api/v2/main-service/badge/list/${id}`);
     if (res.status === 200) {
       return res.data;
     }
@@ -70,7 +70,7 @@ async function fetchGroupBadges(id = 1) {
 async function fetchBoardListByPage(groupId: number = 1, page: number) {
   try {
     const res = await api.get(
-      `/api/v1/main-service/article/${groupId}/articles?offset=${page}`
+      `/api/v2/main-service/article/${groupId}/articles?offset=${page}`
     );
     if (res.status === 200) {
       return res.data;
@@ -86,7 +86,7 @@ async function fetchBoardListByPage(groupId: number = 1, page: number) {
 async function fetchBoardData(groupId: number, articleId: number) {
   try {
     const res = await api.get(
-      `/api/v1/main-service/article/${groupId}/${articleId}`
+      `/api/v2/main-service/article/${groupId}/${articleId}`
     );
     if (res.status === 200) {
       return res.data;
@@ -102,7 +102,7 @@ async function fetchBoardData(groupId: number, articleId: number) {
 async function fetchCommentData(articleId: number, page: number) {
   try {
     const res = await api.get(
-      `/api/v1/main-service/article/${articleId}/comments?offset=${page}`
+      `/api/v2/main-service/article/${articleId}/comments?offset=${page}`
     );
     if (res.status === 200) {
       return res.data;
@@ -118,7 +118,7 @@ async function fetchCommentData(articleId: number, page: number) {
 async function postBoard(groupId: number, board: Board) {
   try {
     const res = await api.post(
-      `/api/v1/main-service/article/${groupId}`,
+      `/api/v2/main-service/article/${groupId}`,
       board
     );
     if (res.status === 200) {
@@ -137,7 +137,7 @@ async function postBoard(groupId: number, board: Board) {
 async function handleMember(groupId: number, status: string, userName: string) {
   try {
     const res = await api.patch(
-      `/api/v1/main-service/group/${groupId}/manage/members/${status}?user_name=${userName}`
+      `/api/v2/main-service/group/${groupId}/manage/members/${status}?user_name=${userName}`
     );
     if (res.status === 200) {
       if (status === "MANAGER") {
@@ -162,7 +162,7 @@ async function handleGroupApplication(
 ) {
   try {
     const res = await api.post(
-      `/api/v1/main-service/group/${groupId}/manage/apply/${status}?user_name=${userName}`
+      `/api/v2/main-service/group/${groupId}/manage/apply/${status}?user_name=${userName}`
     );
     if (res.status === 200) {
       if (status === "ACCEPTED") {
@@ -189,7 +189,7 @@ async function postComment(
 ) {
   try {
     const res = await api.post(
-      `/api/v1/main-service/article/${groupId}/${articleId}/comment`,
+      `/api/v2/main-service/article/${groupId}/${articleId}/comment`,
       { content: content }
     );
     if (res.status === 200) {
@@ -209,7 +209,7 @@ async function postComment(
 async function deleteComment(articleId: number, commentId: number) {
   try {
     const res = await api.delete(
-      `/api/v1/main-service/article/${articleId}/${commentId}`
+      `/api/v2/main-service/article/${articleId}/${commentId}`
     );
     if (res.status === 200) {
       alert("댓글 삭제가 완료되었습니다.");
@@ -232,7 +232,7 @@ async function editBoard(
 ) {
   try {
     const res = await api.patch(
-      `/api/v1/main-service/article/${groupId}/modify/${articleId}`,
+      `/api/v2/main-service/article/${groupId}/modify/${articleId}`,
       {
         title: title,
         content: content,
@@ -255,7 +255,7 @@ async function editBoard(
 async function deleteBoard(groupId: number, articleId: number) {
   try {
     const res = await api.delete(
-      `/api/v1/main-service/article/${groupId}/delete/${articleId}`
+      `/api/v2/main-service/article/${groupId}/delete/${articleId}`
     );
     if (res.status === 200) {
       alert("게시글 삭제가 완료되었습니다.");
