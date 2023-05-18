5ef7f5fb38630ba88d2310b4c4c24f202d306dbf
dan <dankim0213@gmail.com>
2023-05-19 01:04:34 +0900

    fe/feat: footer 추가

--- frontend/src/components/common/Footer.tsx
+++ frontend/src/components/common/Footer.tsx
@@ -1,14 +1,44 @@
+import { Space, Typography } from "antd";
 import { Footer } from "antd/es/layout/layout";
 import styled from "styled-components";
 
 export default function CustomFooter() {
-  return <StyledFooter>Footer</StyledFooter>;
+  return (
+    <StyledFooter>
+      <Space style={{ marginRight: "3rem" }}>
+        <Typography.Text>성장하는 개발자가 되기 위한</Typography.Text>
+        <Typography.Title level={3}>66 Days</Typography.Title>
+        <Typography.Text>간의 여정 </Typography.Text>
+      </Space>
+      <Space style={{ marginRight: "3rem" }}>
+        <Typography.Text>made by</Typography.Text>
+        <Typography.Title level={3}>A705</Typography.Title>
+        <Typography.Text>FE - 김태원 성다연 손예지</Typography.Text>
+        <Typography.Text>BE - 권성은 김진호 박귀렬</Typography.Text>
+      </Space>
+      <Space>
+        <Typography.Text>in </Typography.Text>
+        <Typography.Title level={3}>SSAFY</Typography.Title>
+      </Space>
+    </StyledFooter>
+  );
 }
 
 const StyledFooter = styled(Footer)`
-  text-align: center;
+  display: flex;
+  justify-content: center;
   color: #fff;
   background-color: #6d6d6d;
   padding-inline: 5rem;
-  font-size: 1.6rem;
+
+  h1,
+  h2,
+  h3 {
+    margin: 0;
+    color: white;
+  }
+
+  span {
+    color: white;
+  }
 `;
