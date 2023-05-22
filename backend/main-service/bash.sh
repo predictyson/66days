0497e0469a31a332af7a732c403d0022f5032d0c
권성은 <isungun@naver.com>
2023-05-22 11:25:23 +0900

    Update bash.sh

--- backend/main-service/bash.sh
+++ backend/main-service/bash.sh
@@ -6,4 +6,4 @@ if [ $( docker ps -a | grep ${CONTAINER_NAME} | wc -l ) -gt 0 ]; then
 fi
 
 docker build -t ${CONTAINER_NAME} .
-docker run -d -v /home/ubuntu/image:/home/ubuntu/image --name ${CONTAINER_NAME} -p 8083:8080 ${CONTAINER_NAME}
+docker run -d -v /home/ubuntu/images:/home/ubuntu/images --name ${CONTAINER_NAME} -p 8083:8080 ${CONTAINER_NAME}
